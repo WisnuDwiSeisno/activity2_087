19f23c16852ac94fd6228f922a369a47fd242b26
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-03-08 12:07:09 +0700

    First Commit

---
+++ latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    // begin
+    // Numeric Luas, P, L
+    double Luas, P, L;
+    // Display "Masukan Panjangnya = "
+    cout << "Masukan Panjangnya = ";
+    // Accept P
+    cin >> P;
+    // Display "Masukan Lebarnya = "
+    cout << "Masukan Lebarnya = ";
+    // Accept L
+    cin >> L;
+    // Compute P*L
+    Luas = P*L;
+    // Display "Luasnya = " + Luas
+    cout << "Luasnya = " << Luas;
+} //End
